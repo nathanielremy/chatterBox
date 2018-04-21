c3a53b4d1c44c9e1c2c325704846dbf5d6cf7b5f
Nathaniel Remy <nathanielremy1@gmail.com>
2018-04-21 20:48:45 +0200

    First commit

--- ChatterBox/AppDelegate.swift
+++ ChatterBox/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        window = UIWindow()
+        
+        guard let window = window else { fatalError() }
+        
+        window.rootViewController = LoginVC()
+        
         return true
     }
 
---
+++ ChatterBox/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  ChatterBox
+//
+//  Created by Nathaniel Remy on 21/04/2018.
+//  Copyright © 2018 Nathaniel Remy. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+        
+    }
+}
+
