f32f83809d3cbe8f314d0dfa12f53adfb0054178
Nathaniel Remy <nathanielremy1@gmail.com>
2018-05-02 22:25:32 +0200

    User can now login after loging out

--- ChatterBox/Controllers/LoginVC.swift
+++ ChatterBox/Controllers/LoginVC.swift
@@ -11,6 +11,9 @@ import Firebase
 
 class LoginVC: UIViewController {
     
+    //MARK: Stored properties
+    var stackViewHeight: CGFloat = 150
+    
     let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.image = #imageLiteral(resourceName: "gameofthrones_splash").withRenderingMode(.alwaysOriginal)
@@ -18,6 +21,29 @@ class LoginVC: UIViewController {
         
         return iv
     }()
+    
+    lazy var segmentedControl: UISegmentedControl = {
+        let sc = UISegmentedControl(items: ["Login", "Register"])
+        sc.tintColor = .white
+        sc.selectedSegmentIndex = 1
+        sc.addTarget(self, action: #selector(handleSegmentedController), for: .valueChanged)
+        
+        return sc
+    }()
+    
+    @objc fileprivate func handleSegmentedController() {
+        var title: String
+        if segmentedControl.selectedSegmentIndex == 0 {
+            title = "Login"
+            stackViewHeight = 100
+        } else {
+            title = "Register"
+            stackViewHeight = 150
+        }
+        
+        registerButton.setTitle(title, for: .normal)
+        setupStackView()
+    }
     
     let nameTextField: UITextField = {
         let tf = UITextField()
@@ -62,8 +88,30 @@ class LoginVC: UIViewController {
     }()
     
     @objc fileprivate func handleRegisterButton() {
-        guard let inputs = areInputsValid() else {
+        if segmentedControl.selectedSegmentIndex == 1 {
+            registerUser()
+        } else {
+            loginUser()
+        }
+    }
+    
+    fileprivate func loginUser() {
+        guard let inputs = areLoginInputsValid() else {
             print("OOOOps login inputs are not valid"); return
+        }
+        
+        Auth.auth().signIn(withEmail: inputs.email, password: inputs.password) { (_, err) in
+            if let error = err {
+                print("Error loging in: ", error); return
+            }
+            
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    fileprivate func registerUser() {
+        guard let inputs = areInputsValid() else {
+            print("OOOOps register inputs are not valid"); return
         }
         
         Auth.auth().createUser(withEmail: inputs.email, password: inputs.password) { (userr, err) in
@@ -83,9 +131,8 @@ class LoginVC: UIViewController {
                     print("Error uploading user's values to database: ", error)
                 }
                 
-                print("Successfuly uploaded user's values to database.")
+                self.dismiss(animated: true, completion: nil)
             })
-            
         }
     }
     
@@ -93,7 +140,7 @@ class LoginVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor.mainBlue()
         
-        setupViews()
+        setupStackView()
     }
     
     fileprivate func areInputsValid() -> (name: String, email: String, password: String)? {
@@ -105,39 +152,52 @@ class LoginVC: UIViewController {
         return (name, email, password)
     }
     
-    fileprivate func setupViews() {
-        let stackView = UIStackView(arrangedSubviews: [nameTextField, emailTextField, passwordTextField])
+    fileprivate func areLoginInputsValid() -> (email: String, password: String)? {
+        guard let email = emailTextField.text, email.count > 0 else { return nil }
+        guard let password = passwordTextField.text, password.count > 5 else { return nil }
+        
+        return (email, password)
+    }
+    
+    fileprivate func setupStackView() {
+        
+        var UIViewStack: [UIView] = [nameTextField, emailTextField, passwordTextField]
+        
+        if stackViewHeight == 100 {
+            UIViewStack.remove(at: 0)
+        }
+        
+        let stackView = UIStackView(arrangedSubviews: UIViewStack)
         stackView.axis = .vertical
         stackView.spacing = 0
         stackView.distribution = .fillEqually
         
         view.addSubview(stackView)
-        stackView.anchor(top: nil, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: -12, width: nil, height: 150)
+        stackView.anchor(top: nil, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: -12, width: nil, height: stackViewHeight)
         stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
+        setupViews()
+    }
+    
+    fileprivate func setupViews() {
         view.addSubview(registerButton)
-        registerButton.anchor(top: stackView.bottomAnchor, left: stackView.leftAnchor, bottom: nil, right: stackView.rightAnchor, paddingTop: 8, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: nil, height: 50)
+        registerButton.anchor(top: passwordTextField.bottomAnchor, left: passwordTextField.leftAnchor, bottom: nil, right: passwordTextField.rightAnchor, paddingTop: 8, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: nil, height: 50)
         
         view.addSubview(profileImageView)
         profileImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 30, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 150, height: 150)
         profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        view.addSubview(segmentedControl)
+        segmentedControl.anchor(top: profileImageView.bottomAnchor, left: passwordTextField.leftAnchor, bottom: nil, right: passwordTextField.rightAnchor, paddingTop: 68, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: nil, height: 30)
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- ChatterBox/Controllers/MainVC.swift
+++ ChatterBox/Controllers/MainVC.swift
@@ -7,16 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
 class MainVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if Auth.auth().currentUser?.uid == nil {
+            perform(#selector(handleLogout), with: nil, afterDelay: 0)
+        }
+        
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
     }
     
     @objc fileprivate func handleLogout() {
+        
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("Logout error: ", error)
+            return
+        }
+        
         let loginVC = LoginVC()
         present(loginVC, animated: true, completion: nil)
     }
